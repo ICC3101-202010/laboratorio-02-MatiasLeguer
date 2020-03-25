7814e0a80b343d8a5afd24c782ea5c3ecc0cf180
MatiasLeguer <62114519+MatiasLeguer@users.noreply.github.com>
2020-03-25 20:02:35 -0300

    Clase Canción y método Información creado

---
+++ Cancion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Laboratorio2_MatiasLeguer
+{
+    class Cancion
+    {
+        private string nombre;
+        private string album;
+        private string artista;
+        private string genero;
+
+        public Cancion(string nombre, string album, string artista, string genero)
+        {
+            this.nombre = nombre;
+            this.album = album;
+            this.artista = artista;
+            this.genero = genero;
+        }
+
+        public string Informacion()
+        {
+            public string info = "hola";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2_MatiasLeguer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
